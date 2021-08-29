871a20a465ca4a6d7843bea7431d9ba130d87422
keshav <keshav14.885@gmail.com>
2021-08-29 16:59:29 +0530

    initialised the server

--- mongoDb.py
+++ mongoDb.py
@@ -1,13 +1,16 @@
+from flask.wrappers import Response
 import pymongo
 from pymongo import MongoClient
 from bson import Code
+import flask
+from flask import request, json
 
+
+app = flask.Flask(__name__)
 CONNECTION_STRING = "mongodb+srv://Keshav:ThisIsAPassword@studentrecordshackathon.jgtfv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
 # def getCourseLink():
-# def uploadAssignment():
-# def uploadTest():
 # def linkForLecture():
 # def checkAssignment():
 # def giveMarksAssignment():
@@ -38,6 +41,11 @@ sample_student = {
     },
 }
 
+returnCodes = {
+    "alreadyPresent": {"error": "user email already in use"},
+    "success": {"response": "changes made successfully"},
+}
+
 
 def connectDb():
     client = MongoClient(CONNECTION_STRING)
@@ -47,38 +55,59 @@ def connectDb():
     return teacher_db, student_db
 
 
-def newTeacher(teacherName, teacherEmail):
+@app.route("/createNewTeacher", methods=["POST"])
+def newTeacher():
+    args = request.args
+    teacherName = args["teacherName"]
+    teacherEmail = args["teacherEmail"]
     teacher_db, student_db = connectDb()
-    check = teacher_db.find_one({"email":teacherEmail})
-    if(check == None):
-        teacher_db.insert_one({"name": teacherName, "email": teacherEmail})
-        return teacher_db.find_one({"email": teacherEmail})["_id"]
-    else:
-        return "AlreadyPresent" 
+    # Commented for testing
+    # if teacher_db.find_one({"email": teacherEmail}) != None:
+    #     return returnCodes["alreadyPresent"]
+    # else:
+    teacher_db.insert_one({"name": teacherName, "email": teacherEmail})
+    return returnCodes["success"]
 
-    
 
-def newStudent(studentName, studentEmail):
+@app.route("/createNewStudent", methods=["POST"])
+def newStudent():
     teacher_db, student_db = connectDb()
-    check = student_db.find_one({"email":studentEmail})
-    if(check == None):
-        student_db.insert_one({"name": studentName, "email": studentEmail})
-        return student_db.find_one({"email": studentEmail})["_id"]
-    else:
-        return "AlreadyPresent"
-    
+    args = request.args
+    studentName = args["studentName"]
+    studentEmail = args["studentEmail"]
+    # Commented for testing
+    # if student_db.find_one({"email": studentEmail}) != None:
+    #     return returnCodes["alreadyPresent"]
+    # else:
+    student_db.insert_one({"name": studentName, "email": studentEmail})
+    return returnCodes["success"]
 
-def getTeacherId(teacherEmail):
+
+@app.route("/getTeacherId", methods=["GET"])
+def getTeacherId():
+    args = request.args
+    teacherEmail = args["teacherEmail"]
+
     teacher_db, student_db = connectDb()
-    return teacher_db.find_one({"email": teacherEmail})["_id"]
+    return returnCodes["success"]
 
 
+@app.route("/getStudentId", methods=["GET"])
 def getStudentId(studentEmail):
+    args = request.args
+    studentEmail = args["studentEmail"]
     teacher_db, student_db = connectDb()
-    return student_db.find_one({"email": studentEmail})["_id"]
+    return returnCodes["success"]
 
 
-def createNewCourse(teacherId, courseName, schedule, students):
+@app.route("/createNewCourse", methods=["POST"])
+def createNewCourse():
+    args = request.args
+    teacherId = args["teacherId"]
+    courseName = args["courseName"]
+    data = request.get_json()
+    schedule = data["schedule"]
+    students = data["students"]
     teacher_db, student_db = connectDb()
     student_arr = []
     if len(students) != 0:
@@ -103,18 +132,15 @@ def createNewCourse(teacherId, courseName, schedule, students):
             }
         },
     )
+    return returnCodes["success"]
 
 
-def addTask(teacherId, course, time, questions, taskName):
-    # taskname can be quiz or assignment
-    teacher_db, student_db = connectDb()
-    teacher_db.update_one(
-        {"_id": teacherId},
-        {"$set": {"courses": {course: {"schedule": {taskName: {time: questions}}}}}},
-    )
-
-
-def addStudentToCourse(teacherId, courseName, studentId):
+@app.route("/addStudentToCourse", methods=["POST"])
+def addStudentToCourse():
+    args = request.args
+    teacherId = args["teacherId"]
+    courseName = args["courseName"]
+    studentId = args["studentId"]
     teacher_db, student_db = connectDb()
     student_db.update_one(
         {"_id": studentId},
@@ -132,12 +158,41 @@ def addStudentToCourse(teacherId, courseName, studentId):
             }
         },
     )
+    return returnCodes["success"]
 
 
-def getCoursesList(teacherId):
+@app.route("/getCoursesList", methods=["GET"])
+def getCoursesList():
+    args = request.args
+    teacherId = args["teacherId"]
     teacher_db, student_db = connectDb()
     result = teacher_db.find_one({"_id": teacherId})
-    # return result["courses"]["courseList"]
+    courses = []
+    for course in result["courses"]:
+        courses.append(course)
+    return courses
+
+
+@app.route("/addTask", methods=["POST"])
+def addTask():
+    # taskname can be quiz or assignment
+    args = request.args
+    teacherId = args["teacherId"]
+    courseName = args["courseName"]
+    time = args["time"]
+    taskName = args["taskName"]
+    data = request.get_json()
+    questions = data
+    teacher_db, student_db = connectDb()
+    teacher_db.update_one(
+        {"_id": teacherId},
+        {
+            "$set": {
+                "courses": {courseName: {"schedule": {taskName: {time: questions}}}}
+            }
+        },
+    )
+    return returnCodes["success"]
 
 
 # def addStudentsCourse(emailId):
@@ -145,7 +200,4 @@ def getCoursesList(teacherId):
 #     student_id = student_db.find_one({"email": emailId})["_id"]
 
 
-teacher_id = newTeacher("keshav", "example")
-newStudent("keshav", "nayaEmail.com")
-createNewCourse(teacher_id, "hindi", "schedule", ["nayaEmail.com"])
-getCoursesList(teacher_id)
+app.run(port="5001")
